f1bc0e2f541c24d94995c813dd1e1c82ac65bf7e
Tuigun Bekishev <tbekishev@gmail.com>
2022-10-19 10:47:02 -0300

    Assignment is done

---
+++ middle.js
@@ -0,0 +1,12 @@
+const middle = function(array) {
+  let newArray = [];
+  if (array.length === 1 || array.length === 2) {
+    return newArray;
+  } else if (array.length % 2 === 0) {
+      newArray.push(array[(array.length / 2) - 1]);
+      newArray.push(array[(array.length / 2)]);
+    } else {
+      newArray.push(array[Math.floor((array.length / 2))]);
+    }
+    return newArray;
+}
