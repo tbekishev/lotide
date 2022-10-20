fe90e69b3894f8e7a1e068f67e27483fb6f8fd39
Tuigun Bekishev <tbekishev@gmail.com>
2022-10-19 23:20:37 -0300

    Assignment is done

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  sentence = sentence.split('');
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [];
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+  return results;
+};
+
+const eqArrays = function(array1, array2) {
+  let counter = true;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        counter = false;
+        break;
+      }
+    }
+  } else {
+    counter = false;
+  } return counter;
+};
+
+const assertArraysEqual = function(arrayA, arrayB) {
+  if (eqArrays(arrayA, arrayB)) {
+    console.log(`✅✅✅Assertion Passed: [${arrayA}] === [${arrayB}]`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: [${arrayA}] !== [${arrayB}]`);
+  }
+};
+
+console.log(letterPositions("lighthouse in the house"));
