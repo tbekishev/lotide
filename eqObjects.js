99063acc1930b14e7116654ff9279a8877046e35
Tuigun Bekishev <tbekishev@gmail.com>
2022-10-23 14:00:22 -0300

    eqObjects is updated

--- eqObjects.js
+++ eqObjects.js
@@ -1,15 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqObjects = function(object1, object2) {
   let result = 0;
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (let key in object1) {
+      if (typeof object1[key] === 'object' && typeof object2[key] === 'object')
+        return eqObjects(object1[key], object2[key]);
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         if (eqArrays(object1[key], object2[key])) {
           result++;
@@ -39,10 +33,3 @@ const eqArrays = function(array1, array2) {
     counter = false;
   } return counter;
 };
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
