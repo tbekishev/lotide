aa2641f261ecb54c6096d24a8c77a01be0faeab4
Tuigun Bekishev <tbekishev@gmail.com>
2022-10-24 18:32:38 -0300

    version 1.0.1

--- assertArraysEqual.js
+++
@@ -1,9 +0,0 @@
-const eqArrays = require('./eqArrays');
-const assertArraysEqual = function(arrayA, arrayB) {
-  if (eqArrays(arrayA, arrayB)) {
-    console.log(`✅✅✅Assertion Passed: [${arrayA}] === [${arrayB}]`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: [${arrayA}] !== [${arrayB}]`);
-  }
-};
-module.exports = assertArraysEqual;
--- assertEqual.js
+++
@@ -1,8 +0,0 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-module.exports = assertEqual;
